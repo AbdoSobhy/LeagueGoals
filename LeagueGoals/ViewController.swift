fbe69e117e5c0ed70d0298b2248c09eaedb525fc
AbdoSobhy <AbdoAhmedSobhy@hotmail.com>
2020-07-01 23:57:08 +0200

    first commit

---
+++ LeagueGoals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeagueGoals
+//
+//  Created by Abdelrahman Sobhy on 7/1/20.
+//  Copyright © 2020 Macbook pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# League Goals
+League Goals make it easy for people to trace sports programs and easy for participants (and their families) to get the most out of their sports experiences
