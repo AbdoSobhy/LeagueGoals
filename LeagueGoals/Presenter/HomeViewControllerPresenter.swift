7372ee572aae9308bb5bb407f91310c182649da3
AbdoSobhy <AbdoAhmedSobhy@hotmail.com>
2020-07-02 15:39:56 +0200

    Presenting data and UI enhancements

--- LeagueGoals/Presenter/HomeViewControllerPresenter.swift
+++ LeagueGoals/Presenter/HomeViewControllerPresenter.swift
@@ -11,15 +11,46 @@ protocol HomeViewControllelrView : AnyObject {
     
 }
 protocol HomeViewControllerPresenter {
-    
+    var leaugesCount: Int { get }
+
+    func configure(cell : LeaugeCellView , row : Int)
+    func getLeauges(completionHandler : @escaping ([Leagues])->Void)
+
 }
 class HomeViewControllerPresenterImpl {
     weak var view : HomeViewControllelrView?
+    
+    var leauges = [Leagues]()
+    
     
     init(view : HomeViewControllelrView) {
         self.view = view
     }
 }
-extension HomeViewControllerPresenterImpl : HomeViewControllerPresenter{
+extension HomeViewControllerPresenterImpl : HomeViewControllerPresenter {
+    
+    var leaugesCount: Int {
+        leauges.count
+    }
+    
+    func getLeauges(completionHandler: @escaping ([Leagues]) -> Void) {
+        LeagueRequest.leagueRequest(request: LeagueRouter.getLeagues) { (leauges : League?) in
+            if let leauges = leauges {
+                self.leauges.append(contentsOf: leauges.leagues)
+                completionHandler(leauges.leagues)
+            }
+            else {
+                // show error to user
+            }
+        }
+    }
+
+        // Cell Configure
+    func configure(cell : LeaugeCellView , row : Int){
+        cell.display(leagueName: self.leauges[row].strLeague ?? "")
+        cell.display(leagueShortName: self.leauges[row].strLeagueAlternate ?? "")
+        cell.display(leagueSport: self.leauges[row].strSport ?? "")
+    }
     
 }
+
--- LeagueGoals/View/League Screen/HomeViewController.swift
+++ LeagueGoals/View/League Screen/HomeViewController.swift
@@ -9,14 +9,34 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak private var leaugeTableView: UITableView!
     var homeViewControllerPresenter : HomeViewControllerPresenter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         homeViewControllerPresenter = HomeViewControllerPresenterImpl(view : self)
-        // Do any additional setup after loading the view.
+        homeViewControllerPresenter?.getLeauges(completionHandler: { (leagues) in
+            if !leagues.isEmpty {
+                self.leaugeTableView.reloadData()
+            }
+        })
     }
     
+}
+
+extension HomeViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.homeViewControllerPresenter?.leaugesCount ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "LeaugeTableViewCell") as? LeaugeTableViewCell else { return UITableViewCell() }
+        self.homeViewControllerPresenter?.configure(cell: cell, row: indexPath.row)
+        return cell
+    }
+    
+}
+extension HomeViewController : UITableViewDelegate {
     
 }
 extension HomeViewController : HomeViewControllelrView {
